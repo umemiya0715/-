8c1128a112d6e4fc74427a792b9814bca3cc3f9a
umemiya0715 <whiteblue116@icloud.com>
2021-07-28 23:35:38 +0900

    modified_header_to_hamburger button

--- app/javascript/components/TheHeader.vue
+++ app/javascript/components/TheHeader.vue
@@ -1,34 +1,43 @@
 <template>
   <header>
-    <div class='bg-gray-200'>
-      <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-        <div class="text-gray-500 text-sm text-right font-light sm:text-right">
+    <div class='w-full text-gray-700 bg-gray-200'>
+      <div class='flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8'>
+        <div class='flex flex-row items-center justify-between p-1'>
+          <h1>HR</h1>
+          <button @click='isOpen = !isOpen' class='md:hidden rounded-lg focus:outline-none focus:shadow-outline'>
+            <svg class='h-6 w-6 fill-current' viewBox='0 0 24 24'>
+              <path v-show='!isOpen' d='M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z' />
+              <path v-show='isOpen' d='M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z' />
+            </svg>
+          </button>
+        </div>
+        <nav :class="isOpen ? 'block' : 'hidden'" class='flex-col flex-grow md:pb-0 md:flex md:justify-end md:flex-row'>
           <router-link
-            to="/register"
-            class="nav-link"
+            to='/register'
+            class='block px-8 py-2 my-4 hover:bg-gray-600 rounded'
           >
             ユーザー登録
           </router-link>
           <router-link
-            to="/login"
-            class="nav-link"
+            to='/login'
+            class='block px-8 py-2 my-4 hover:bg-gray-600 rounded'
           >
             ログイン
           </router-link>
-        </div>
+        </nav>
       </div>
-    </div>
+    </div> 
   </header>
 </template>
 
 <script>
 export default {
-  name: "TheHeader",
-  computed: {
+  name: 'TheHeader',
+  data() {
+    return {
+      isOpen: false,
+    }
   },
-  methods: {
-    
-  }
 }
 </script>
 
