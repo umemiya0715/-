5563690bb9c7876dc829649ea3b98648dd9ce32d
umemiya0715 <whiteblue116@icloud.com>
2021-08-03 00:27:52 +0900

    add_twitter_gem

---
+++ app/controllers/api/v1/analysis_controller.rb
@@ -0,0 +1,13 @@
+class Api::Vi::AnalysisController < ApplicationController
+
+  private
+
+  def twitter_client
+    @client ||= Twitter::REST::Client.new |config|
+      config.consumer_key = Rails.application.credentials.twitter[:consumer_key]
+      config.consumer_secret = Rails.application.credentials.twitter[:consumer_secret]
+      config.access_token = Rails.application.credentials.twitter[:access_token]
+      config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
+    end
+  end
+end
--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-8 bg-red-500 bg-opacity-50">
+  <div class="mt-8 bg-red-300 bg-gray-200">
     <div class="text-center">
       <h3>{{ title }}</h3>
     </div>
@@ -7,7 +7,7 @@
       <form class="grid grid-cols-1 gap-6 m-16">
         <div class="mt-4">
           <input type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="@dragon">
-        </div>  
+        </div>
       </form>
       <router-link to="/result" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">診断する</router-link>
     </div>
