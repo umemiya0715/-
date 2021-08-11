6f58314db5b2dc925aa050b54ebb71b7fb0952b9
umemiya0715 <whiteblue116@icloud.com>
2021-08-12 01:05:24 +0900

    add_accountdata_controller

---
+++ app/controllers/api/v1/accountdata_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class AccountdataController < ApplicationController
+      before_action :set_accountdata, only: %i[show update destroy]
+
+      def index
+        @accountdata = Accountdata.all
+        render json: @accountdata
+      end
+
+      def show
+        render json: @accountdata
+      end
+
+      def create
+        @accountdata = Accountdata.new(accountdata_params)
+        @accountdata = @accountdata.fetchdata
+        if @accountdata.save
+          render json: @accountdata
+        else
+          render json: @accountdata.errors, status: :bad_request
+        end
+      end
+
+      private
+
+      def set_accountdata
+        @accountdata = accountdata.find(params[:id])
+      end
+
+      def accountdata_params
+        params.require(:accountdata).permit(:accountdata)
+      end
+    end
+  end
+end
--- app/controllers/api/v1/results_controller.rb
+++ app/controllers/api/v1/results_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ResultsController < ApplicationController
-      before_action :set_result, only: %i[show]
+      before_action :set_result, only: %i[show update destroy]
 
       def index
         @result = Result.all
@@ -12,10 +12,24 @@ module Api
         render json: @result
       end
 
+      def create
+        @result = Result.new(result_params)
+        @result = @result.analyze
+        if @result.save
+          render json: @result
+        else
+          render json: @result.errors, status: :bad_request
+        end
+      end
+
       private
 
       def set_result
         @result = Result.find(params[:id])
+      end
+
+      def result_params
+        params.require(:result).permit(:result)
       end
     end
   end
