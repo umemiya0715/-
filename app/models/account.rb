54c67de9cc4bb2b85a81ee82d698513c97a01875
umemiya0715 <whiteblue116@icloud.com>
2022-03-25 20:35:51 +0900

    changed_gcs_credentials_to_local_place

--- app/models/account.rb
+++ app/models/account.rb
@@ -16,7 +16,7 @@ class Account < ApplicationRecord
   def sentimentAnalyze(user, tweets)
     require "google/cloud/language"
     client = Google::Cloud::Language.language_service do |config|
-      config.credentials = "/app/config/gcs_credentials.json"
+        config.credentials = "/Users/umemiyashouta/Downloads/dragon-twitter-analysis.json"
     end
     resultScore = []
     resultMagnitude = []
