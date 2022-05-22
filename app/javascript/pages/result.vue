b44471edc15d71fb5326de589ae6465d4771735c
umemiya0715 <whiteblue116@icloud.com>
2022-05-22 20:01:37 +0900

    improved_result_and_dragon_pages

--- app/javascript/pages/result.vue
+++ app/javascript/pages/result.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="text-center">
-    <div class="grid grid-cols-12 gap-10 md:p-20">
+    <div class="grid grid-cols-12 gap-10 md:pt-20">
       <div class="col-start-1 col-span-12 mt-20 md:mt-0">
-        <div class="text-2xl inline p-5 text-white font-bold border-b-8 border-white md:text-4xl">{{ title }}</div>
+        <div class="text-2xl inline p-2 text-white font-bold border-b-8 border-white md:text-4xl">{{ title }}</div>
       </div>
-      <div class="text-center text-white col-start-1 col-span-12 p-10 mx-auto" v-if="dragons.attributes">
-        <div class="text-2xl font-bold p-4">
+      <div class="text-center text-white col-start-1 col-span-12 px-10 pb-10 mx-auto" v-if="dragons.attributes">
+        <div class="text-6xl font-bold">
           <h3>{{ dragons.attributes.name }}</h3>
         </div>
-        <div class="m-auto p-4">
+        <div class="m-auto">
           <img :src="dragon_image_src">
         </div>
-        <div class="text-lg p-4 m-4 border-2 border-white rounded-md font-bold mx-16">
-          <div class="text-2xl inline font-bold border-b-2 border-white">性格</div>
-          <div class="m-4">{{ dragons.attributes.explanation }}</div>
-          <div class="m-4">{{ dragons.attributes.personality }}</div>
+        <div class="p-4 m-4 border-4 border-white rounded-md font-bold mx-16">
+          <div class="text-3xl inline font-bold border-b-2 border-white">性格</div>
+          <div class="text-3xl m-4">{{ dragons.attributes.explanation }}</div>
+          <div class="text-2xl m-4">{{ dragons.attributes.personality }}</div>
         </div>
-        <div class="text-lg p-4 m-4 border-2 border-white rounded-md font-bold mx-16">
-          <div class="text-2xl inline font-bold border-b-2 border-white">アドバイス</div>
-          <div class="m-4">{{ dragons.attributes.advice }}</div>
+        <div class="p-4 m-4 border-4 border-white rounded-md font-bold mx-16">
+          <div class="text-3xl inline font-bold border-b-2 border-white">アドバイス</div>
+          <div class="text-2xl m-4">{{ dragons.attributes.advice }}</div>
         </div>
       </div>
     </div>
     <div class="text-center p-4">
-      <button class="bg-twitterBlue hover:bg-blue-700 text-white text-lg font-bold py-4 px-4 rounded" @click="twitterShare" >
+      <button class="bg-twitterBlue hover:bg-blue-700 text-white text-2xl font-bold py-4 px-4 rounded" @click="twitterShare" >
         <i class="px-2 ri-twitter-fill"></i>Twitterでシェアする
       </button>
     </div>
-    <div class="text-center text-white p-8 text-lg">
+    <div class="text-center text-white p-8 text-2xl">
       <router-link to="/">トップページに戻る</router-link>
     </div>
   </div>
