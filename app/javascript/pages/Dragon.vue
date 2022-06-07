3b15fb9fff916de1a74ce32fe577e694b8f36f1c
umemiya0715 <whiteblue116@icloud.com>
2022-06-07 17:55:45 +0900

    fixed_result_design

--- app/javascript/pages/Dragon.vue
+++ app/javascript/pages/Dragon.vue
@@ -4,21 +4,21 @@
       <div class="col-start-2 col-span-10 mt-20 md:mt-0">
         <div class="text-2xl inline p-2 text-white font-bold border-b-8 border-white md:text-4xl">{{ title }}</div>
       </div>
-      <div class="text-center text-white col-start-2 col-span-10 px-10 pb-10 mx-auto" v-if="this.dragon.attributes">
+      <div class="text-center text-white col-start-2 col-span-10 px-10 pb-10 mx-auto" v-if="dragon.attributes">
         <div class="text-6xl font-bold">
-          <h3>{{ this.dragon.attributes.name }}</h3>
+          <h3>{{ dragon.attributes.name }}</h3>
         </div>
         <div class="m-auto">
           <img :src="dragon_image_src">
         </div>
-        <div class="p-4 m-4 border-4 border-white rounded-md font-bold mx-16">
+        <div class="p-4 m-4 border-4 border-white rounded-md font-bold mx-4">
           <div class="text-3xl inline font-bold border-b-2 border-white">性格</div>
-          <div class="text-3xl m-4">{{ this.dragon.attributes.explanation }}</div>
-          <div class="text-2xl m-4">{{ this.dragon.attributes.personality }}</div>
+          <div class="text-3xl m-4">{{ dragon.attributes.explanation }}</div>
+          <div class="text-2xl m-4 text-left">{{ dragon.attributes.personality }}</div>
         </div>
-        <div class="p-4 m-4 border-4 border-white rounded-md font-bold mx-16">
+        <div class="p-4 m-4 border-4 border-white rounded-md font-bold mx-4">
           <div class="text-3xl inline font-bold border-b-2 border-white">アドバイス</div>
-          <div class="text-2xl m-4">{{ this.dragon.attributes.advice }}</div>
+          <div class="text-2xl m-4 text-left">{{ dragon.attributes.advice }}</div>
         </div>
       </div>
     </div>
