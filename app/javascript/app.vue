de3e4704dd36e9308f8633f4330792488b2fa16e
umemiya0715 <whiteblue116@icloud.com>
2022-06-13 19:22:17 +0900

    implemented_flash_messages

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class='d-flex flex-column min-h-screen bg-cover bg-fixed bg-center pt-32' :style="{ backgroundImage: 'url(' + image_src + ')' }">
+  <div class='d-flex flex-column min-h-screen bg-cover bg-fixed bg-center pt-28' :style="{ backgroundImage: 'url(' + image_src + ')' }">
     <TheHeader class='mb-auto' />
+      <transition>
+        <TheFlash v-if="flash.message" />
+      </transition>
       <router-view />
     <TheFooter class='mt-auto' />
   </div>
@@ -9,18 +12,35 @@
 <script>
 import TheHeader from 'components/TheHeader'
 import TheFooter from 'components/TheFooter'
+import TheFlash from 'components/TheFlash'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
     TheHeader,
     TheFooter,
+    TheFlash
   },
   methods: {
   },
   computed: {
+    ...mapGetters(
+      'flash',['flash']
+    ),
     image_src() {
       return require("../../public/images/topImage.png")
     }
   }
 }
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ app/javascript/components/TheFlash.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-if="flash.type == 'info'" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded fixed top-28 w-full">
+      {{ flash.message }}
+    </div>
+    <div v-if="flash.type == 'alert'" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded fixed top-28 w-full">
+      {{ flash.message }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  data() {
+    return {
+      isShow: true
+    }
+  },
+  computed: {
+    ...mapGetters({ flash: "flash/flash" })
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$store.dispatch("flash/fetchFlash",{
+        type: "",
+        message: ""
+      })
+    }, 2000)
+  }
+}
+</script>
+
+<style scoped>
+</style>
