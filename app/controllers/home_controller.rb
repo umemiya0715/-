1861683e89131470c4a12e7c7a6681102e5f5857
umemiya0715 <whiteblue116@icloud.com>
2022-06-15 16:44:41 +0900

    request_spec(user_sessions_test)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,2 @@
 class HomeController < ApplicationController
-  def index; end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'factory_bot'
+require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -66,6 +68,7 @@ RSpec.configure do |config|
 
   # skip NAmespace to use FactoryBot
   config.include FactoryBot::Syntax::Methods
+  config.include TestSessionsHelper
 
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
