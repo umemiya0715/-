6023300436a9ddf29dfa44c2e825f3c5d9f58c84
umemiya0715 <whiteblue116@icloud.com>
2022-06-21 16:55:50 +0900

    updated_help_page

--- app/javascript/pages/Help.vue
+++ app/javascript/pages/Help.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="text-center min-h-screen">
     <div class="grid grid-cols-12 gap-10 md:pt-20">
-      <div class="text-center text-white col-start-2 col-span-10">
-        <div class="text-4xl inline-block p-2 font-bold border-b-8 border-white mb-10">
+      <div class="text-center text-black bg-white rounded col-start-2 col-span-10 p-4">
+        <div class="text-4xl inline-block p-2 font-bold border-b-8 border-twitterBlue mt-4 mb-10">
           ドラッターって何？
         </div>
-        <p class="text-3xl col-start-2 col-span-10 text-left text-white">最近のTwitterの運用方法について振り返ることができるアプリです。</p>
+        <p class="text-3xl p-4 text-left">最近のTwitterの運用方法について振り返ることができるアプリです。</p>
       </div>
 
-      <div class="text-center text-white col-start-2 col-span-10">
-        <div class="text-4xl inline-block p-2 font-bold border-b-8 border-white mb-10">使い方</div>
-        <div class="text-3xl text-left">
-          <p>トップページの入力画面に診断したいアカウントのIDを入力。</p>
-          <p>「診断する」ボタンを押して、診断結果が返ってくるまで少しの間待機。</p>
-          <p>ツイート内容や使い方に応じて、診断対象のアカウントの心の様子をドラゴンで表してくれます。</p>
-          <p>Twitterアカウントでユーザー登録をすると、過去に行った診断結果を振り返ることができます。</p>
+      <div class="text-center text-black bg-white rounded col-start-2 col-span-10 p-4">
+        <div class="text-4xl inline-block p-2 font-bold border-b-8 border-twitterBlue mt-4 mb-10">
+          使い方
+        </div>
+        <div v-for="(description, index) in descriptions" :key="index" class="flex">
+          <div class="text-3xl text-left p-4">{{ description.text }}</div>
+          <img :src="description.imgSrc" height="400px" width="300px" class="p-4">
         </div>
       </div>
 
-      <div class="text-center text-white col-start-2 col-span-10">
-        <div class="text-4xl inline-block p-2 font-bold mb-10 border-b-8 border-white">何でドラゴン？</div>
-        <div class="text-3xl text-left">
+      <div class="text-center text-black bg-white rounded col-start-2 col-span-10 p-4">
+        <div class="text-4xl inline-block p-2 font-bold border-b-8 border-twitterBlue mt-4 mb-10">何でドラゴン？</div>
+        <div class="text-3xl text-left p-4">
           <p>ドラゴン(竜・龍）は伝説上の生き物で、人間の想像力が生み出した存在です。</p>
           <p>西洋では悪や力の化身であり征服されるものとして、東洋では神や自然の象徴として畏怖される存在として、古来から人の歴史と共に歩んできました。</p>
           <p>今現在でも様々なファンタジー小説やゲームなどに登場し、花形として人気のあるキャラクターです。</p>
@@ -44,7 +44,24 @@ export default {
   name: "Help",
   data() {
     return {
-      title: "このサイトは何なのか？"
+    }
+  },
+  computed: {
+    descriptions() {
+      return [
+        {
+          imgSrc: "/images/howToImage1.jpg",
+          text: "トップページの入力画面に診断したいアカウントのIDを入力。「診断する」ボタンを押して、診断結果が返ってくるまで少しの間待機。"
+        },
+        {
+          imgSrc: "/images/howToImage2.jpg",
+          text: "ツイート内容や使い方に応じて、診断対象のアカウントの心の様子をドラゴンで表してくれます。"
+        },
+        {
+          imgSrc: "/images/howToImage3.jpg",
+          text: "Twitterアカウントでユーザー登録をすると、過去に行った診断結果を振り返ることができます。"
+        }
+      ]
     }
   }
 }
