3da34fe44b9d2268b26afeadee58f6b1b8960b6a
umemiya0715 <whiteblue116@icloud.com>
2022-07-03 17:32:09 +0900

    displayed_result_data_in_charts

--- app/javascript/components/TheCharts.vue
+++ app/javascript/components/TheCharts.vue
@@ -1,19 +1,14 @@
 <template>
-  <Bar class="bg-white p-4 rounded"
+  <Bar
     :chart-options="chartOptions"
     :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
+    :v-bind="results"
   />
 </template>
 
 <script>
 import { Bar } from 'vue-chartjs/legacy'
+import { mapGetters } from 'vuex'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
@@ -21,45 +16,35 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
   name: 'BarChart',
   components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: 'bar-chart'
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {}
-    },
-    plugins: {
-      type: Array,
-      default: () => []
-    }
-  },
   data() {
     return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
       chartOptions: {
         responsive: true,
-        indexAxis: 'y'
+        label: false,
+        indexAxis: 'y',
+        scales: {
+          x: {
+            max: 1,
+          }
+        }
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(
+      'results', ['results']
+    ),
+    chartData() {
+      return {
+        labels: [ 'ポジティブ度', '感情の強さ', '活発度' ],
+        datasets: [
+          {
+            data: [this.results.score, this.results.magnitude, this.results.troversion],
+            backgroundColor: 'rgba(168, 218, 236, 0.2)',
+            borderColor: 'rgba(28,171,236,1)',
+            borderWidth: 2
+          }
+        ]
       }
     }
   }
