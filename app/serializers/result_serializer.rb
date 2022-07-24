2803b25f0239a992c7a9b26e52ec244b5d487c80
umemiya0715 <whiteblue116@icloud.com>
2022-07-24 18:01:56 +0900

    fixed_result_serializer

--- app/serializers/result_serializer.rb
+++ app/serializers/result_serializer.rb
@@ -1,5 +1,5 @@
 class ResultSerializer < ActiveModel::Serializer
-  attributes :id, :dragon_id, :user_id, :target_account, :score, :magnitude, :troversion, :created_at
+  attributes :id, :dragon_id, :user_id, :target_account, :screen_name, :score, :magnitude, :troversion, :created_at
 
   belongs_to :dragon
 end
