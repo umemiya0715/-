1396bc9ee8863e0730d97a4a302b2a7e80a91bb0
umemiya0715 <whiteblue116@icloud.com>
2022-08-06 23:53:28 +0900

    updated_rails_from_6.0_to_6.1

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,25 +11,12 @@ module EmotionDragon
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # config.i18n.default_locale = :ja
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    # config.time_zone = 'Tokyo'
-    # config.active_record.default_timezone = :local
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
-    config.generators do |g|
-      g.stylesheets false
-      g.javascript false
-      g.helper false
-      g.template_engine false
-      g.test_framework nil
-    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
